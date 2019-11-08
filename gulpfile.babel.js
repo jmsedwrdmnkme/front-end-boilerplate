1712dd7fb874fb25be08ea42366246ebe332c9b3
James Monk <info@jamesmonk.me>
2019-11-08 10:29:14 +0000

    Removal of Bootstrap and several other dependencies, beginning to rework front end

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -188,22 +188,8 @@ function jsnoncritical() {
   return gulp
     .src([
       './node_modules/jquery/dist/jquery.slim.js',
-      './node_modules/popper.js/dist/umd/popper.js',
       './node_modules/jquery-lazy/jquery.lazy.js',
       './node_modules/jquery-lazy/jquery.lazy.plugins.js',
-      './node_modules/bootstrap/js/dist/util.js',
-      './node_modules/bootstrap/js/dist/alert.js',
-      './node_modules/bootstrap/js/dist/button.js',
-      './node_modules/bootstrap/js/dist/carousel.js',
-      './node_modules/bootstrap/js/dist/collapse.js',
-      './node_modules/bootstrap/js/dist/dropdown.js',
-      './node_modules/bootstrap/js/dist/index.js',
-      './node_modules/bootstrap/js/dist/modal.js',
-      './node_modules/bootstrap/js/dist/scrollspy.js',
-      './node_modules/bootstrap/js/dist/tab.js',
-      './node_modules/bootstrap/js/dist/toast.js',
-      './node_modules/bootstrap/js/dist/tooltip.js',
-      './node_modules/bootstrap/js/dist/popover.js',
       './src/js/main.js'
     ], { allowEmpty: true })
     .pipe(
