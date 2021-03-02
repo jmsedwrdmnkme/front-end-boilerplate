a805fd768009e874c565d1e5f5df41da7fbe10c7
James Monk <james@jamesmonk.me>
2021-03-02 11:33:04 +0100

    Added logging for Critical in case of errors. Update to PostCSS dependency,  as required by NPM process.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,6 +23,7 @@ const favicon = require('gulp-base64-favicon');
 const browsersync = require('browser-sync').create();
 const purgecss = require('gulp-purgecss');
 const critical = require('critical').stream;
+const log = require('fancy-log');
 
 
 //
@@ -90,17 +91,17 @@ function jsnoncritical() {
   return gulp
     .src([
       './node_modules/popper.js/dist/umd/popper.js',
-      './node_modules/bootstrap/js/dist/alert',
-      './node_modules/bootstrap/js/dist/button',
-      './node_modules/bootstrap/js/dist/carousel',
-      './node_modules/bootstrap/js/dist/collapse',
-      './node_modules/bootstrap/js/dist/dropdown',
-      './node_modules/bootstrap/js/dist/modal',
-      './node_modules/bootstrap/js/dist/popover',
-      './node_modules/bootstrap/js/dist/scrollspy',
-      './node_modules/bootstrap/js/dist/tab',
-      './node_modules/bootstrap/js/dist/toast',
-      './node_modules/bootstrap/js/dist/tooltip',
+      './node_modules/bootstrap/js/dist/alert.js',
+      './node_modules/bootstrap/js/dist/button.js',
+      './node_modules/bootstrap/js/dist/carousel.js',
+      './node_modules/bootstrap/js/dist/collapse.js',
+      './node_modules/bootstrap/js/dist/dropdown.js',
+      './node_modules/bootstrap/js/dist/modal.js',
+      './node_modules/bootstrap/js/dist/popover.js',
+      './node_modules/bootstrap/js/dist/scrollspy.js',
+      './node_modules/bootstrap/js/dist/tab.js',
+      './node_modules/bootstrap/js/dist/toast.js',
+      './node_modules/bootstrap/js/dist/tooltip.js',
       './src/js/main.js',
       './node_modules/fg-loadcss/dist/loadCSS.js',
       './node_modules/vanilla-lazyload/dist/lazyload.js'
