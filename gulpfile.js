6a1b8bcb4a2944895be466e7ab80c23913626216
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2026-08-17 11:42:59 +0100

    Update for legacy SASS errors as well as various QoL updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
-import {src, dest, watch, series, parallel} from 'gulp';
-import {deleteAsync} from 'del';
+import { src, dest, watch, series, parallel } from 'gulp';
+import { deleteAsync } from 'del';
 import purgecss from 'gulp-purgecss';
-import {stream as critical} from 'critical';
+import { stream as critical } from 'critical';
 import compiler from 'webpack';
 import webpack from 'webpack-stream';
 import strip from 'gulp-strip-comments';
 import concat from 'gulp-concat';
-import dartSass from 'sass';
+import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
 import uglify from 'gulp-uglify';
 import svgsprite from 'gulp-svg-sprite';
-import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
+import imagemin, { gifsicle, mozjpeg, optipng, svgo } from 'gulp-imagemin';
 import webp from 'gulp-webp';
 import hb from 'gulp-hb';
 import ext from 'gulp-ext-replace'
 import sitemap from 'gulp-sitemap';
 import browsersync from 'browser-sync';
 
-export const clean = () => deleteAsync('dist/');
+const clean = () => deleteAsync('dist/');
 
-export function root() {
-  return src(['src/root/*', 'src/root/.*'], {encoding: false})
+function root() {
+  return src(['src/root/*', 'src/root/.*'], { encoding: false })
     .pipe(dest('dist/'))
     .pipe(browsersync.stream());
 }
 
-export function fonts() {
-  return src('src/fonts/*', {encoding: false})
+function fonts() {
+  return src('src/fonts/*', { encoding: false })
     .pipe(dest('dist/fonts/'))
     .pipe(browsersync.stream());
 }
 
-export function videos() {
-  return src('src/videos/*', {encoding: false})
+function videos() {
+  return src('src/videos/*', { encoding: false })
     .pipe(dest('dist/videos/'))
     .pipe(browsersync.stream());
 }
 
-export function scripts() {
-  return src('src/js/**/*.js', {encoding: false})
+function scripts() {
+  return src('src/js/**/*.js', { encoding: false })
     .pipe(webpack({}, compiler, function() {}))
     .pipe(uglify())
     .pipe(concat('main.js'))
@@ -47,10 +47,10 @@ export function scripts() {
     .pipe(browsersync.stream());
 }
 
-export function styles() {
-  return src('src/css/main.scss', {encoding: false})
+function styles() {
+  return src('src/css/main.scss', { encoding: false })
     .pipe(sass({
-      silenceDeprecations: ['legacy-js-api', 'color-functions', 'global-builtin', 'import'],
+      silenceDeprecations: ['legacy-js-api', 'color-functions', 'global-builtin', 'import', 'if-function'],
       style: 'compressed'
     }).on('error', sass.logError))
     .pipe(concat('main.css'))
@@ -60,26 +60,29 @@ export function styles() {
         standard: [/:/]
       }
     }))
-    .pipe(dest('dist/css/'))
+    .pipe(dest('src/css/compiled/'))
     .pipe(browsersync.stream());
 }
 
-export function criticalStyles() {
-  return src('dist/**/*.html', {encoding: false})
+function criticalStyles() {
+  return src('dist/*.html', { encoding: false })
     .pipe(
       critical({
         inline: true,
         base: 'dist/',
+        css: 'src/css/compiled/main.css',
         width: 1300,
-        height: 900
+        height: 900,
+        target: { uncritical: 'css/main.css' },
+        extract: true
       })
     )
     .pipe(dest('dist/'))
     .pipe(browsersync.stream());
 }
 
-export function sprite() {
-  return src('src/sprite/**/**/*.svg', {encoding: false})
+function sprite() {
+  return src('src/sprite/**/**/*.svg', { encoding: false })
     .pipe(svgsprite({
       shape: { spacing: { padding: 5 } },
       mode: { symbol: true },
@@ -90,12 +93,12 @@ export function sprite() {
     .pipe(browsersync.stream());
 }
 
-export function images() {
-  src('src/img/**/**/*[.jpg|.gif|.png]', {encoding: false})
+function images() {
+  src('src/img/**/**/*[.jpg|.gif|.png]', { encoding: false })
     .pipe(imagemin([
-      gifsicle({interlaced: true}),
-      mozjpeg({quality: 75, progressive: true}),
-      optipng({optimizationLevel: 5}),
+      gifsicle({ interlaced: true }),
+      mozjpeg({ quality: 75, progressive: true }),
+      optipng({ optimizationLevel: 5 }),
     ]))
     .pipe(webp())
     .pipe(dest('dist/img/'))
@@ -123,37 +126,32 @@ export function images() {
     .pipe(browsersync.stream());
 }
 
-export function html() {
-  return src('src/html/*.hbs', {encoding: false})
+function html() {
+  return src('src/html/*.hbs', { encoding: false })
     .pipe(hb().partials('src/html/partials/**/*.hbs'))
     .pipe(ext('.html'))
     .pipe(dest('dist/'))
     .pipe(browsersync.stream());
 }
 
-export function sitemaps() {
-  return src('dist/*.html', {
-    encoding: false,
-    read: false
-  })
+function sitemaps() {
+  return src('dist/*.html', { encoding: false, read: false })
     .pipe(sitemap({
       siteUrl: 'localhost',
       fileName: 'sitemap.xml',
       changefreq: 'weekly',
-      priority: function(siteUrl, loc, entry) {
-        return loc.split('/').length === 0 ? 1 : 0.5;
-      }
+      priority: function(siteUrl, loc, entry) { return loc.split('/').length === 0 ? 1 : 0.5; }
     }))
     .pipe(dest('dist/'))
     .pipe(browsersync.stream());
 }
 
-export function browserSync(done) {
-  browsersync.init({server: {baseDir: "dist"}, port: 3000, open: false});
+function browserSync(done) {
+  browsersync.init({ server: { baseDir: "dist" }, port: 3000, open: false });
   done();
 }
 
-export function browserSyncReload(done) {
+function browserSyncReload(done) {
   browsersync.reload();
   done();
 }
@@ -164,6 +162,8 @@ function watchFiles() {
   watch(['src/html/**/*.hbs', 'src/css/**/*.scss'], htmlBuild);
   watch('src/img/**/*', images);
   watch('src/root/**/*', root);
+  watch('src/videos/*', videos);
+  watch('src/fonts/*', fonts);
 }
 
 const htmlBuild = series(html, styles, criticalStyles, sitemaps);
